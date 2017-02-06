121b676a18f52065d9f708b76da8d1b9e8b8c034
Sean Nam <namsangwoo1@gmail.com>
2017-02-05 23:04:20 -0800

    Added poster images using UIImageView and the AFNetworking library. Installed using Cocoapods

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell
+//  Flicks
+//
+//  Created by Sean Nam on 2/5/17.
+//  Copyright © 2017 Sean Nam. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -7,13 +7,12 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     var movies: [NSDictionary]? = []
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,14 +55,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath);
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
         
-        cell.textLabel!.text = title;
+        let baseUrl = "https://image.tmdb.org/t/p/w500/"
+        let posterPath = movie["poster_path"] as! String
         
-        print("row \(indexPath.row)")
+        let imageUrl = NSURL(string: baseUrl + posterPath)
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        cell.posterView.setImageWith(imageUrl! as URL)
         
         return cell
     }
